d41a715bbab0059b05aaf9a10f77da5b25435e93
Anderson Marques <andersonmarques.ci@gmail.com>
2016-11-15 22:09:05 -0300

    Primeiros commits

---
+++ src/main/java/br/com/loginAngular/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package br.com.loginAngular.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "br.com.loginAngular.controller" })
+public class MvcConfig {
+
+}
---
+++ src/main/java/br/com/loginAngular/config/ServiceConfig.java
@@ -0,0 +1,10 @@
+package br.com.loginAngular.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.loginAngular.service")
+public class ServiceConfig {
+
+}
