2536cbdb933d1842abec7913943d1f147f65da95
Anderson Marques <andersonmarques.ci@gmail.com>
2016-11-21 10:18:15 -0300

    Adicionado angular no projeto

--- src/main/java/br/com/loginAngular/config/MvcConfig.java
+++ src/main/java/br/com/loginAngular/config/MvcConfig.java
@@ -4,13 +4,20 @@ import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("br.com.loginAngular.controller")
-public class MvcConfig {
+public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	private static final Logger log = Logger.getLogger(MvcConfig.class);
 	
@@ -25,4 +32,26 @@ public class MvcConfig {
 	    resolver.setSuffix(".jsp");
 	    return resolver;
 	}
+	
+	@Bean
+    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+        return new ResourceUrlEncodingFilter();
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
+                .setCachePeriod(0)
+                .resourceChain(true)
+                .addResolver(new GzipResourceResolver())
+                .addResolver(new PathResourceResolver());
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
 }
--- src/main/webapp/views/home.jsp
+++ src/main/webapp/views/home.jsp
@@ -1,9 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Página de Login Angular</title>
-</head>
+<html lang="pt-br" ng-app="app">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Login AngularJS</title>
+        <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+        <link rel="stylesheet" href="resources/css/bootstrap-theme.min.css">
+        <script src="resources/js/lib/angular.min.js"></script>
+        <script src="resources/js/main.js"></script>
+    </head>
 <body>
     <h1>Home</h1>
 </body>
